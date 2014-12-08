d517444295cda52b1d4287b1de170f65671ae101
EricKreate <eric@kreate.ca>
2014-12-08 15:57:10 -0500

    =wired front end interface to k-api for entry and display of users table items

--- js/app.js
+++ js/app.js
@@ -3,21 +3,26 @@ var kreate = angular.module('kreate', ['ui.router', 'ngCookies', 'ngMaterial']);
 kreate.config(function ($stateProvider, $urlRouterProvider) {
 
   // Set default 'fallback' route
- // $urlRouterProvider.otherwise('/');
+  // $urlRouterProvider.otherwise('/');
 
   // Define states/routes
   $stateProvider
   // Main template state
     .state('createuser', {
-    url: '/createuser',
-    templateUrl: 'views/createuser.html',
-    controller: 'CreateUserController'
-  })
+      url: '/createuser',
+      templateUrl: 'views/createuser.html',
+      controller: 'CreateUserController'
+    })
+    .state('userlist', {
+      url: '/userlist',
+      templateUrl: 'views/userlist.html',
+      controller: 'UserListController'
+    })
     .state('createapp', {
-    url: '/createapp',
-    templateUrl: 'views/createapp.html',
-    controller: 'CreateAppController'
-  });
+      url: '/createapp',
+      templateUrl: 'views/createapp.html',
+      controller: 'CreateAppController'
+    });
 });
 
 // Run immediately - this is like the 'main' block.
@@ -27,9 +32,9 @@ kreate.run(['$rootScope', '$location',
   }
 ]);
 
-kreate.controller('CreateUserController', function ($scope, $state) {
+kreate.controller('CreateUserController', function ($scope, $state, $http) {
   $scope.user = {
-    id: -1,
+    id: null,
     email: '',
     password: '',
     firstname: '',
@@ -37,11 +42,46 @@ kreate.controller('CreateUserController', function ($scope, $state) {
     apps: '{}',
     notes: ''
   };
+  $scope.createUser = function () {
+    var req = {
+      method: 'POST',
+      url: 'http://localhost:3000/v0/users',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: $scope.user
+    };
+
+    $http(req).success(function (data, status, headers, config) {
+      alert('success!');
+      $location.kpath('#/index.html');
+    }).error(function (data, status, headers, config, statusText) {
+      alert('error:' + status + ' ' + statusText);
+    });
+  };
+});
+
+kreate.controller('UserListController', function ($scope, $state, $http) {
+  var req = {
+    method: 'GET',
+    url: 'http://localhost:3000/v0/users',
+    headers: {
+      'Content-Type': 'application/json'
+    } //,
+    //  data: $scope.user
+  };
+  console.log('here');
+  $http(req).success(function (data, status, headers, config) {
+    $scope.users = data;
+    //  $location.kpath('#/userlist.html');
+  }).error(function (data, status, headers, config, statusText) {
+    alert('error:' + status + ' ' + statusText);
+  });
 });
 
 kreate.controller('CreateAppController', function ($scope, $state) {
   $scope.app = {
-    id: -1,
+    id: null,
     customer: '',
     title: '',
     database: '',
